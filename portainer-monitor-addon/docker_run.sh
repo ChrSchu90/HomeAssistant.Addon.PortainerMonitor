e9c9c1bb7f81dc9b020df24652c29c6c9f13396f
ChrSchu <chr.schu@outlook.com>
2025-05-24 20:12:38 +0200

    Fixed error on standalone addon startup due to missing Supervisor API

--- portainer-monitor-addon/docker_run.sh
+++ portainer-monitor-addon/docker_run.sh
@@ -2,9 +2,8 @@
 cd "$(dirname "$0")"
 
 DOCKER_FILE="Dockerfile"
-CONTAINER_DIR="."
 IMAGE_NAME="portainer-monitor-addon:dev"
-BUILD_VERSION=1.0.0
+BUILD_VERSION=0.0.1
 BUILD_ARCH=amd64
 BUILD_FROM=ghcr.io/home-assistant/amd64-base
 
@@ -13,6 +12,5 @@ NET_RUNTIME=linux-musl-x64
 NET_OUT=./source/bin/publish/${BUILD_ARCH}
 
 dotnet publish ${NET_PROJECT} -r ${NET_RUNTIME} -p:DebugType=embedded -p:PublishSingleFile=true --verbosity normal --configuration Release --self-contained -o ${NET_OUT}
-
-docker build --no-cache --progress=plain --pull --rm --build-arg BUILD_VERSION="${BUILD_VERSION}" --build-arg BUILD_ARCH=${BUILD_ARCH} --build-arg BUILD_FROM=${BUILD_FROM} -f ${DOCKER_FILE} -t ${IMAGE_NAME} ${CONTAINER_DIR} && \
-    docker run --rm -it -v ./debug/data:/data ${IMAGE_NAME}
+docker build --no-cache --progress=plain --pull --rm --build-arg BUILD_VERSION="${BUILD_VERSION}" --build-arg BUILD_ARCH=${BUILD_ARCH} --build-arg BUILD_FROM=${BUILD_FROM} -f ${DOCKER_FILE} -t ${IMAGE_NAME}
+docker run --rm -it -e PORTAINER_MONITOR_CONFIG="$(cat ./source/Properties/DebugConfig.json)" ${IMAGE_NAME}
--- portainer-monitor-addon/run.sh
+++ portainer-monitor-addon/run.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/with-contenv bashio
 
-MqttUser=$(bashio::services "mqtt" "username")
-MqttPw=$(bashio::services "mqtt" "password")
-MqttHost=$(bashio::services "mqtt" "host")
-MqttPort=$(bashio::services "mqtt" "port")
-
-./HomeAssistant.Addon.PortainerMonitor --mqttuser "${MqttUser}" --mqttpw "${MqttPw}" --mqtthost "${MqttHost}" --mqttport "${MqttPort}"
+if [[ -v PORTAINER_MONITOR_CONFIG ]]; then
+    echo "Portainer Monitor is starting in standalone mode..."
+    ./HomeAssistant.Addon.PortainerMonitor
+else
+    echo "Portainer Monitor is starting in supervised mode..."
+    MqttUser=$(bashio::services "mqtt" "username")
+    MqttPw=$(bashio::services "mqtt" "password")
+    MqttHost=$(bashio::services "mqtt" "host")
+    MqttPort=$(bashio::services "mqtt" "port")
+    ./HomeAssistant.Addon.PortainerMonitor --mqttuser "${MqttUser}" --mqttpw "${MqttPw}" --mqtthost "${MqttHost}" --mqttport "${MqttPort}"
+fi
